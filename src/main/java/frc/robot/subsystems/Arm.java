aea5fea79a192c4a0692378816f0c4b2d263cfec
Kev1n8088 <jebidiahkerman828@gmail.com>
2024-11-07 18:03:09 -0500

    added motor controller and encoder declarations for arm subsystem

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,5 +1,18 @@
 package main.java.frc.robot.subsystems;
 
 public class Arm {
+        private CANSparkMax armMotor = new CANSparkMax(
+                        Constants.Drivetrain.Motor.CAN_IDs.LEFT_MOTOR, MotorType.kBrushless);
+        private CANSparkMax intakeMotor = new CANSparkMax(
+                        Constants.Drivetrain.Motor.CAN_IDs.RIGHT_MOTOR, MotorType.kBrushless);
+        private CANSparkMax shooterMotor = new CANSparkMax(
+                        Constants.Drivetrain.Motor.CAN_IDs.RIGHT_MOTOR, MotorType.kBrushless);
+
+        /*
+         * Define and initialize arm, intake, and shooter encoders. 
+         */
+        private RelativeEncoder armEncoder = armMotor.getEncoder();
+        private RelativeEncoder intakeEncoder = intakeMotor.getEncoder();
+        private RelativeEncoder shooterEncoder = shooterMotor.getEncoder();
     
 }
